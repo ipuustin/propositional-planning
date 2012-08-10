76e43eabe0f96bb5e9f7962232749b5a505f9107
Ismo Puustinen <ismo@iki.fi>
2012-08-10 23:23:46 +0300

    Started working on successor-state axioms -- doesn't work correctly ATM

--- Satplan.hs
+++ Satplan.hs
@@ -7,6 +7,9 @@ import Debug.Trace as Trace
 -- package "hatt"
 import Data.Logic.Propositional as Prop
 
+-- package "incremental-sat-solver"
+import Data.Boolean.SatSolver
+
 type Cost = Int
 type Precondition = Expr
 type Effect = Expr
@@ -30,6 +33,14 @@ data Action = Action String [Precondition] [Effect] Cost
 -- must take to complete the plan, or Impossible if the goal could not
 -- be reached.
 data Plan = Plan [Action] | Impossible deriving (Show)
+
+-- The problem is the initial state, list of possible actions, and the
+-- desired goal state
+data Problem = Problem [Expr] [Action] [Expr]
+
+type VariableMapping = [(Int, Expr)]
+
+type LeveledVariable = (Int, Expr)
 
 -- helper functions
 
@@ -74,6 +85,72 @@ cnfdist e = e -- negations and variables
 
 tocnf :: Expr -> Expr
 tocnf e = cnfdist $ cnfreplace e
+
+
+
+
+tolvar :: Show a => a -> Expr -> Expr
+tolvar x (Negation a) = Negation (tolvar x a)
+tolvar x (Conditional a b) = Conditional (tolvar x a) (tolvar x b)
+tolvar x (Biconditional a b) = Biconditional (tolvar x a) (tolvar x b)
+tolvar x (Conjunction a b) = Conjunction (tolvar x a) (tolvar x b)
+tolvar x (Disjunction a b) = Disjunction (tolvar x a) (tolvar x b)
+tolvar x (Variable a) = Variable $ a ++ "_" ++ (show x)
+
+
+findpredicates :: ActionData a => [a] -> [Expr]
+findpredicates as = List.nub $ foldl getpreds [] as
+    where getpreds acc a = acc ++ (preconditions a) ++ (effects a)
+
+
+gatherconjunction :: [Expr] -> Expr
+gatherconjunction (a:[]) = a
+gatherconjunction [] = Variable "TODO"
+gatherconjunction (a:as) = foldl Conjunction a as
+
+gatherdisjunction :: [Action] -> Expr
+gatherdisjunction (a:[]) = Variable $ name a
+gatherdisjunction [] = Variable "TODO"
+gatherdisjunction (a:as) = foldl Disjunction (Variable $ name a) (map (Variable . name) as)
+
+createsuccessorstateaxiom :: Expr -> [Action] -> [Action] -> Int -> Expr
+createsuccessorstateaxiom f doers undoers t = axiom
+    where prev = t - 1
+          actioncauses f = gatherdisjunction doers
+          actiondeletes = gatherdisjunction undoers
+          axiom = Biconditional (tolvar t f) (tolvar prev $ Disjunction (actioncauses f) (Disjunction f (Negation actiondeletes)))
+
+
+
+findsuccessors :: [Action] -> [Expr] -> Int -> Int -> Expr
+findsuccessors as fs time currenttime = expression
+    where axioms = foldl getfluentaxiom [] fs
+          getfluentaxiom acc f = (createsuccessorstateaxiom f (doers f) (undoers f) currenttime) : acc
+          doers f = filter (hasineffects f) as
+          undoers f = filter (hasineffects (Negation f)) as
+          hasineffects f a = any (== f) (effects a)
+          expression = gatherconjunction axioms
+
+findaxioms :: [Expr] -> [Action] -> Int -> Expr
+findaxioms predicates actions time = Conjunction successorexpr (Conjunction preconditionexpr exclusionexpr)
+    where successorexpr = findsuccessors actions predicates time 1
+          preconditionexpr = undefined
+          exclusionexpr = undefined
+
+-- create the CNF and the mapping from the problem
+
+translatetosat :: Problem -> Int -> (Expr, VariableMapping)
+translatetosat (Problem initials actions goals) time = (cnfexpr, mapping)
+    where i:is = map (tocnf . tolvar 0) initials
+          g:gs = map (tocnf . tolvar time) goals
+          startexpr = foldl Conjunction i is
+          successorexpr = undefined
+          goalexpr = foldl Conjunction g gs
+          cnfexpr = Conjunction startexpr (Conjunction successorexpr goalexpr)
+          mapping = []
+          predicates = findpredicates actions
+-- successor-state axioms, precondition axioms, action exclusion axioms
+
 
 
 -- testing
