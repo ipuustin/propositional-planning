40a57d2a5f432a01228eca87b54fc7334a5c95a2
Ismo Puustinen <ismo@iki.fi>
2012-08-13 00:51:18 +0300

    Added support for exclusion expressions and precondition expressions.

--- Satplan.hs
+++ Satplan.hs
@@ -59,28 +59,21 @@ cnfreplace (Negation a) = case a of
                 Conjunction i j -> Disjunction (cnfreplace (Negation i)) (cnfreplace (Negation j))
                 Disjunction i j -> Conjunction (cnfreplace (Negation i)) (cnfreplace (Negation j))
                 _ -> Negation $ cnfreplace a
-
 cnfreplace (Conditional a b) = cnfreplace $ Disjunction (Negation a) b
-
 cnfreplace (Biconditional a b) = cnfreplace $ Conjunction (Conditional a b) (Conditional b a) 
-
 cnfreplace (Conjunction a b) = Conjunction (cnfreplace a) (cnfreplace b)
-
 cnfreplace (Disjunction a b) = Disjunction (cnfreplace a) (cnfreplace b)
-
 cnfreplace (Variable x) = Variable x
 
 -- finally distribute disjunction over conjunction wherever possible
 
 cnfdist (Conjunction a b) = Conjunction (cnfdist a) (cnfdist b)
-
 cnfdist (Disjunction a b) = dist a b
     where dist (Conjunction i j) (Conjunction k l) =
                     Disjunction (cnfdist $ Conjunction i j) (cnfdist $ Conjunction k l)
           dist (Conjunction i j) k = Conjunction (cnfdist $ Disjunction i k) (cnfdist $ Disjunction j k)
           dist i (Conjunction j k) = Conjunction (cnfdist $ Disjunction i j) (cnfdist $ Disjunction i k)
           dist a b = Disjunction (cnfdist a) (cnfdist b)
-
 cnfdist e = e -- negations and variables
 
 tocnf :: Expr -> Expr
@@ -128,6 +121,7 @@ createsuccessorstateaxiom f doers undoers t = let
                 (Nothing, Just ad) -> Biconditional (tolvar next f) (tolvar t $ Disjunction f (Negation ad))
                 (Nothing, Nothing) -> Biconditional (tolvar next f) (tolvar t f)
 
+-- TODO: can be Nothing
 findsuccessors :: [Action] -> [Expr] -> Int -> Expr
 findsuccessors as fs t = let
           axioms = foldl getfluentaxiom [] fs
@@ -138,12 +132,38 @@ findsuccessors as fs t = let
     in
           gatherconjunction axioms
 
+-- effects of one action are in contradiction with preconditions of another action
+interference :: Action -> Action -> Bool
+interference a1 a2 =
+    let ef1 = effects a1
+        ef2 = effects a2
+        pre1 = preconditions a1
+        pre2 = preconditions a2
+    in any Prop.isContradiction [Conjunction ex1 ex2 | ex1 <- ef1, ex2 <- pre2]
+        || any Prop.isContradiction [Conjunction ex1 ex2 | ex1 <- pre1, ex2 <- ef2]
+
+-- TODO: can be Nothing
+findexclusions :: [Action] -> Expr
+findexclusions actions = let
+          mutexes = [(a1, a2) | a1 <- actions, a2 <- actions, a1 /= a2 && interference a1 a2]
+          pair (a1, a2) = Negation $ Conjunction (Variable $ name a1) (Variable $ name a2)
+    in
+          gatherconjunction $ map pair mutexes
+
+
+-- TODO: can be Nothing
+findpreconditions :: [Action] -> Expr
+findpreconditions as = let
+          axioms = [Conditional (Variable $ name a) (gatherconjunction $ preconditions a) | a <- as, not . null $ preconditions a]
+    in
+          gatherconjunction axioms
+
 findallsuccessors :: [Action] -> [Expr] -> Int -> Expr
 findallsuccessors as fs tmax = let
-          preconditionexpr t = undefined
-          exclusionexpr t = undefined
+          preconditionexpr = findpreconditions as
+          exclusionexpr = findexclusions as
           successorexpr t = findsuccessors as fs t
-          exprsatlevel t = Conjunction (successorexpr t) (Conjunction (preconditionexpr t) (exclusionexpr t))
+          exprsatlevel t = Conjunction (successorexpr t) (Conjunction (tolvar t $ preconditionexpr) (tolvar t $ exclusionexpr))
           allexprs = [exprsatlevel x | x <- [0 .. tmax]]
     in
           gatherconjunction allexprs
