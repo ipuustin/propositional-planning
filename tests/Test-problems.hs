30acdb461c8845f451f6cb220f7883ce2ca4ff98
Ismo Puustinen <ismo@iki.fi>
2013-05-19 12:11:19 +0300

    Started implementing block world test case.

--- tests/Test-problems.hs
+++ tests/Test-problems.hs
@@ -1,5 +1,7 @@
+import AI.Planning
 import AI.Planning.SatPlan
 import Criterion.Main
+import Data.List
 
 -- flashlight domain, expected outcome:
 -- Just [(0,"RemoveCap()"),(1,"Insert(Battery1)"),(1,"Insert(Battery2)"),(1,"Insert(Battery3)"),(1,"Insert(Battery4)"),(2,"PlaceCap()")]
@@ -72,8 +74,77 @@ goalstate3 = [Negation (Variable "Effect()")]
 prob3 = Problem initialstate3 actions3 goalstate3
 
 
+
+assignVariables :: String -> [String] -> String
+assignVariables fn vs = fn ++ "(" ++ (intercalate "," vs) ++ ")"
+
+-- block world -- (http://icaps06.icaps-conference.org/dc-papers/paper5.pdf)
+
+-- FIXME: the Negation(Variable("In()")) clauses are missing
+
+emptySlot :: [String] -> String -> [Expr]
+emptySlot bs s = [ Negation $ Variable ("In("++b++","++s++")") | b <- bs ]
+
+setBoxToSlot bs s b = boxInSlot : otherBoxes
+    where boxInSlot = Variable ("In("++b++","++s++")")
+          otherBoxes = [ Negation $ Variable ("In("++ob++","++s++")") | ob <- bs, ob /= b ]
+
+emptyHandler bs = [ Negation $ Variable ("Holding("++b++")") | b <- bs ]
+
+setBoxToHandler b bs = boxInHandler : otherBoxes
+    where boxInHandler = Variable ("Holding("++b++")")
+          otherBoxes = [ Negation $ Variable ("Holding("++ob++")") | ob <- bs, ob /= b ]
+
+boxes = generateVariables "box" ['a'..'b']
+slots = generateVariables "slot" [1..3]
+
+getBox a = head $ generateVariables "box" [a]
+getSlot x = head $ generateVariables "slot" [x]
+
+bwactions = pickups ++ putdowns
+    where pickups  = [ Action ("Pickup("++b++","++s++")")
+                             (pickupPreconditions s b)
+                             (pickupEffects s b)
+                             1
+                    | b <- boxes, s <- slots ]
+          putdowns = [ Action ("Putdown("++b++","++s++")")
+                             (putdownPreconditions s b)
+                             (putdownEffects s b)
+                             1
+                    | b <- boxes, s <- slots ]
+
+          pickupPreconditions s b = setBoxToSlot boxes s b ++ emptyHandler boxes
+          pickupEffects s b = emptySlot boxes s ++ setBoxToHandler b boxes
+
+          putdownPreconditions s b = setBoxToHandler b boxes ++ emptySlot boxes s
+          putdownEffects s b = setBoxToSlot boxes s b ++ emptyHandler boxes
+
+bwstate1 = emptyHandler boxes ++ slot3 ++ slot2 ++ slot1
+    where slot1 = setBoxToSlot boxes (getSlot 1) (getBox 'a')
+          slot2 = setBoxToSlot boxes (getSlot 2) (getBox 'b')
+          slot3 = emptySlot boxes $ getSlot 3
+
+bwstate2 = emptyHandler boxes ++ slot3 ++ slot2 ++ slot1
+    where slot1 = setBoxToSlot boxes (getSlot 1) (getBox 'b')
+          slot2 = setBoxToSlot boxes (getSlot 2) (getBox 'a')
+          slot3 = emptySlot boxes $ getSlot 3
+
+bwstate3 = emptyHandler boxes ++ slot3 ++ slot2 ++ slot1
+    where slot1 = setBoxToSlot boxes (getSlot 2) (getBox 'a')
+          slot2 = setBoxToSlot boxes (getSlot 3) (getBox 'b')
+          slot3 = emptySlot boxes $ getSlot 1
+
+-- not working for some reason
+bwprob1 = Problem bwstate1 bwactions bwstate2
+
+bwprob2 = Problem bwstate2 bwactions bwstate3
+
+bwprob3 = Problem bwstate1 bwactions bwstate3
+
 -- main program benchmarks flashlight problem
 
 main = defaultMain [
-		bench "flashlight problem" $ nf (runSat flprob) 10
+		bench "flashlight problem" $ nf (runSat flprob) 10,
+		bench "block world problem 2" $ nf (runSat bwprob2) 10,
+    bench "block world problem 3" $ nf (runSat bwprob3) 10
 	]
